35250fb4e82db55d640d107e16d606245556f01e
Michael Tamatey <160445316+Massmac@users.noreply.github.com>
2025-03-04 11:52:39 -0400

    Add routing and initial components for main application structure

--- resume_comparator_frontend/src/App.jsx
+++ resume_comparator_frontend/src/App.jsx
@@ -1,14 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/navbar/Layout"; 
+import MainPage from "./components/mainPage/MainPage"; 
+import Reports from "./components/pages/reports/Reports";
+import Account from "./components/auth/Account";
+import Summary from "./components/pages/summary/Summary";
 
-import './App.css'
+/*
+ Author: Michael Tamatey
+ Date: 20250304
+ Description: This class controls routes
+*/
 
 function App() {
-
   return (
-
-    <div> 
-      Hello Cv compare App
-    </div>
-  )
+    <Router>
+      <Routes>
+        {/* Wrap all routes inside Layout */}
+        <Route path="/" element={<Layout />}>
+          {/* Default Route */}
+          <Route index element={<MainPage />} />
+          <Route path="reports" element={<Reports />} />
+          <Route path="account" element={<Account />} />
+          <Route path="summary" element={<Summary />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
