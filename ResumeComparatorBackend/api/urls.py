c89c7a3e34caf6f182dbfd2d6b4e96245ab6c63f
Izaak Ford <izaakford30@gmail.com>
2025-03-07 19:03:20 -0400

    Added API.

--- ResumeComparatorBackend/api/urls.py
+++ ResumeComparatorBackend/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from api.views import ItemsView
+
+from api.views.compare_view import CompareView
+from api.views.job_posting_view import JobPostingView
+from api.views.report_view import ReportsView
+from api.views.reports_view import ReportView
 
 urlpatterns = [
-    path('items/', ItemsView.as_view(), name='items'),
+    path('reports/', ReportsView.as_view(), name='reports'),
+    path('reports/<int:uid>/', ReportView.as_view(), name='report'),
+    path('job-postings/', JobPostingView.as_view(), name='job-postings'),
+    path('compare/', CompareView.as_view(), name='compare')
 ]
