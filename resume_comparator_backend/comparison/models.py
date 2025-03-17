2cb215155db561f24120e8afe00f35a84f8afdf3
RillaStudios <rillastudios@outlook.com>
2025-03-17 02:59:19 -0300

    Remove the old backend.

--- resume_comparator_backend/comparison/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-# Create your models here.
-
-class Resume(models.Model):
-    file = models.FileField(upload_to='resumes/')
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
--- resume_comparator_backend/comparison/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import process_text
-
-urlpatterns = [ 
-    path('process_text/', views.process_text, name='process_text'), 
-    ] 
