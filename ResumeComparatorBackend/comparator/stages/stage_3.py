ca4903864fb6e8d8ca6493dfa445a54a40e52f0e
Izaak Ford <izaakford30@gmail.com>
2025-04-09 10:06:48 -0300

    Refactor scoring logic in resume fit function to utilize model similarity scores and improve accuracy

--- ResumeComparatorBackend/comparator/stages/stage_3.py
+++ ResumeComparatorBackend/comparator/stages/stage_3.py
@@ -5,15 +5,12 @@ from sentence_transformers import SentenceTransformer, util
 from api.job_posting.job_posting import JobPosting
 from comparator.compare_utils.ai_tools.pdf_parser import classify_text
 from comparator.resume.resume import Resume
-import re
 from cleantext.sklearn import CleanTransformer
 
 # Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoApp.settings')
 
 django.setup()
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def resume_fit(resume_text: str, job_posting_id: int = None):
 
@@ -30,8 +27,23 @@ def resume_fit(resume_text: str, job_posting_id: int = None):
     skills = cleaner.transform([sections['skills']])[0]
 
     # Extract job requirements
-    if job_posting.job_requirements_must_have is not None:
-        job_skills = ", ".join(str(item) for item in job_posting.job_requirements_must_have)
+    score = model_sim_score(job_posting.job_requirements_must_have, skills)
+
+    print(score)
+
+    e_score = model_sim_score(job_posting.job_requirements_nice_to_have, skills)
+
+    final_score = ((score * 0.7) + (e_score * 0.3))
+
+    print(final_score)
+
+def model_sim_score(req_list: list[str], skills: str):
+
+    # Load the pre-trained SentenceTransformer model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    if req_list is not None:
+        job_skills = ", ".join(str(item) for item in req_list)
 
         compare_skills = [skills, job_skills]
 
@@ -44,7 +56,7 @@ def resume_fit(resume_text: str, job_posting_id: int = None):
         similarity_score = similarities[0, 1].item()  # Convert tensor value to Python float
         similarity_percentage = similarity_score * 100
 
-        print(f"Resume Skills match Job Requirements: {similarity_percentage:.2f}%")
+        return similarity_percentage
 
 resume_path = os.path.join(settings.BASE_DIR, 'media', 'resumes', 'Michael Tettey_Tamatey_Resume..pdf')
 resume = Resume(resume_path)
